1019464ba3a7803111a84a18657994845b7b6dec
lubien <lubien1996@gmail.com>
2019-05-16 02:15:33 -0300

    Format nuxt config

--- config/nuxt.js
+++ config/nuxt.js
@@ -4,47 +4,47 @@ const resolve = require('path').resolve
 
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'R.E.A.C.T.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Requirements Evolution in Agile ContexT' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Requirements Evolution in Agile ContexT'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: ['~assets/css/main.css'],
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#744d82' },
   /*
-  ** Render settings
-  */
+   ** Render settings
+   */
   render: { ssr: false },
   /*
-  ** Point to resources
-  */
+   ** Point to resources
+   */
   srcDir: resolve(__dirname, '..', 'resources'),
   /*
-  ** Plugin settings
-  */
+   ** Plugin settings
+   */
   plugins: [
     { src: '~/plugins/vuex-persist', ssr: false },
     '~/plugins/tour',
     '~/plugins/antd'
   ],
   /*
-  ** Plugin settings
-  */
-  modules: [
-    '@nuxtjs/axios'
-  ]
+   ** Plugin settings
+   */
+  modules: ['@nuxtjs/axios']
 }
